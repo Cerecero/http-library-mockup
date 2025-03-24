928cf6fd47923cecfc42395c9a3cc0d0e78fb389
Cerecero <ale.rey.cer@outlook.com>
2025-03-24 12:56:42 -0600

    added func NewRequest

--- internal/request.go
+++ internal/request.go
@@ -1,13 +1,41 @@
 package internal
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Header struct{ Key, Value string }
 
 type Request struct {
 	Method  string
 	Path    string
-	Headers []struct{ Key, Value string }
+	Headers []Header
 	Body    string
 }
+
+func NewRequest(method, path, host, body string) (*Request, error) {
+	switch {
+	case method == "":
+		return nil, errors.New("missing required argument: method")
+	case path == "":
+		return nil, errors.New("missing required argument: path")
+	case !strings.HasPrefix(path, "/"):
+		return nil, errors.New("path muist start with /")
+	case host == "":
+		return nil, errors.New("missing required argument: host")
+	default:
+		headers := make([]Header, 2)
+		headers[0] = Header{"Host", host}
+		if body != "" {
+			headers = append(headers, Header{"Content-Length", fmt.Sprintf("%d", len(body))})
+		}
+		return &Request{Method: method, Path: path, Headers: headers, Body: body}, nil
+	}
+
+}
+
 // HTTP Request look like this
 // <METHOD> <PATH> <PROTOCOL/VERSION>
 // Host: <Host>
