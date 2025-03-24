967b19d5ed351c043dfb41d24422b1a15fe4ff7b
Cerecero <ale.rey.cer@outlook.com>
2025-03-24 12:36:45 -0600

    added comments

--- internal/response.go
+++ internal/response.go
@@ -5,3 +5,28 @@ type Response struct {
 	Headers []struct {Key, Value string}
 	Body string
 }
+
+// HTTP response look like this
+// <PROTOCOL/VERSION> <STATUS CODE> <STATUS MESSAGE>
+// [<HEADER>: <VALUE>] (optional)
+// [<HEADER>: <VALUE>]
+// [<HEADER>: <VALUE>]
+
+// [<RESPONSE BODY>] (optional)
+
+// For example:
+// HTTP/1.1 200 OK
+// Content-Type: application/json
+// Vary: Origin
+// Vary: Access-Control-Request-Method
+// Vary: Authorization
+// Date: Mon, 24 Mar 2025 18:34:15 GMT
+// Content-Length: 97
+//
+// {
+//         "status": "available",
+//         "system_info": {
+//                 "environment": "development",
+//                 "version": ""
+//         }
+// }
