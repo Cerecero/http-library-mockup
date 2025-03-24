63f15fc706398072343ecee44e2ffe8df86d3c3b
Cerecero <ale.rey.cer@outlook.com>
2025-03-24 15:50:27 -0600

    added more validator functions

--- internal/validator.go
+++ internal/validator.go
@@ -13,11 +13,24 @@ func AsTitle(key string) string {
 	return newTitleCase(key)
 }
 
+func isTitleCase(key string) bool {
+	for i := range key {
+		if i == 0 || key[i-1] == '-'{
+			if key[i] >= 'a' && key[i] <= 'z' {
+				return false
+			}
+		} else if key[i] >= 'A' && key[i] <= 'Z' {
+			return false
+		}
+	}
+	return true
+}
+
 func newTitleCase(key string) string {
 	var b strings.Builder
 	b.Grow(len(key))
 	for i := range key {
-		if i == 0 || key[i-1] == "-" {
+		if i == 0 || key[i-1] == '-' {
 			b.WriteByte(upper(key[i]))
 		} else {
 			b.WriteByte(lower(key[i]))
@@ -34,8 +47,10 @@ func lower(b byte) byte {
 }
 
 func upper(b byte) byte {
-
-	panic("unimplemented")
+	if b >= 'a' && b <= 'z' {
+		return b + 'A' - 'a'
+	}
+	return b
 }
 
 
